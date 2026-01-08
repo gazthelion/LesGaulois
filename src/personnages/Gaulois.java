4eb5911530fd12997601ac622013ab3eae4f1436
gazthelion <albert-zeze.guilavogui@univ-tlse3.fr>
2026-01-08 11:15:56 +0100

    TP1 fin de l'exercice 1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,19 @@
 package personnages;
 
 public class Gaulois {
-
+	//création des attributs //
+	private String nom;
+	private int force;
+	
+	//création du constructeur //
+	
+	public Gaulois(String nom, int force) {
+		this.nom=nom;
+		this.force=force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
 }

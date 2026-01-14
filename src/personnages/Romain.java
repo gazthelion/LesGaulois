8fa1364176d88e1f47575d095f6610523c352886
gazthelion <albert-zeze.guilavogui@univ-tlse3.fr>
2026-01-14 13:02:44 +0100

    TP1 fin de l'exerice 8

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,20 @@
 package personnages;
 
 public class Romain {
-
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	//8.Création de la méthode parler
+	public void parler(String texte) {
+		System.out.println("le romain " + nom + " : « " + texte + " ».");
+	}
+	
 }
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,11 +1,13 @@
 package test_fonctionnel;
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Asterix", 8);
+		Romain minus = new Romain("Minus", 6);
 		System.out.println(asterix.getNom());
 		asterix.parler("Bonjour à tous");
-		
+		minus.parler("UN GAU… UN GAUGAU…");
 	}
 }

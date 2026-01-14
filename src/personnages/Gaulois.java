eb7372e02eccf97f7b5dee7bc9133ce89e963032
gazthelion <albert-zeze.guilavogui@univ-tlse3.fr>
2026-01-14 12:06:51 +0100

    TP1 fin de l'exerice 4

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,20 @@
 package personnages;
 
+//a.Création de la classe 'Gaulois'
 public class Gaulois {
-
+	String nom;
+	int force;
+	
+	//b.Constructeur de la classe
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	//c.un getteur sur le nom
+	public String getNom() {
+		return nom;
+	}
+	
+	
 }
